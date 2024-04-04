a26953927af0defcb390c872ab03cfe1b7e596c0
tsv19326 <146067360+tsv19326@users.noreply.github.com>
2024-04-04 20:27:33 +0300

    Update script-login.js

--- load-testing/script-login.js
+++ load-testing/script-login.js
@@ -1,11 +1,13 @@
 import http from 'k6/http';
 
 export const options = {
-    // A number specifying the number of VUs to run concurrently.
-    vus: 10,
-    // A string specifying the total duration of the test run.
-    duration: '30s',
+    stages: [
+        { duration: '10s', target: 50 }, // Постепенно увеличаване на натоварването
+        { duration: '10s', target: 50 }, // Стабилно натоварване
+        { duration: '10s', target: 0 },  // Намаляване на натоварването
+    ],
 };
+
 
 export default function () {
     const url = 'http://localhost:8080/login';
